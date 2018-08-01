1aa249be18bf26e3d40e4c144cf226dce47c403a
Adrian Yuson <adrianyuson1988@gmail.com>
2018-08-01 19:45:23 +0000

    added google maps

--- routes/gyms.js
+++ routes/gyms.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var NodeGeocoder = require("node-geocoder");
 
 router.get("/gyms", function(req, res) {
    res.render("gyms.ejs"); 
