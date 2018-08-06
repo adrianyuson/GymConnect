7f2100520f544a1ff56c96f75fde9b80519ed2fd
Adrian Yuson <adrianyuson1988@gmail.com>
2018-08-06 22:07:08 +0000

    Add User Model and User Auth Configs

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var commentRoutes = require("./routes/comments.js");
 var indexRoutes = require("./routes/index.js");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var User = require("./models/user.js");
 
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/gymconnect", { useNewUrlParser: true });
@@ -14,6 +17,19 @@ mongoose.connect("mongodb://localhost:27017/gymconnect", { useNewUrlParser: true
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "Time is of the essence",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(gymRoutes);
 app.use(commentRoutes);
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,36 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user.js");
 
+//Landing Page Route
 router.get("/", function(req, res) {
    res.render("index.ejs"); 
 });
 
+//Register Page Route
+router.get("/register", function(req, res) {
+    res.render("register.ejs");
+});
+
+//Register logic
+router.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+       if(err) {
+           console.log(err);
+           return res.send("error yo");
+       }
+       passport.authenticate("local")(req, res, function() {
+          res.redirect("/gyms"); 
+       });
+    });
+});
+
+//Login Page Route
 router.get("/login", function(req, res) {
     res.render("login.ejs");
 });
 
-router.get("/signup", function(req, res) {
-    res.render("signup.ejs");
-});
 
 module.exports = router;
